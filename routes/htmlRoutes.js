0ecc9a7322613a36a144bbe153862bcf7f0a8ca3
Liam B <71141670+liambambery23@users.noreply.github.com>
2021-02-09 11:26:47 -0500

    fixed html routes, added route for each page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,18 @@
-let path = require("path");
+const router = require("express").Router();
+const path = require("path");
 
-module.exports(function(app) {
-    app.get("/exercise", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/exercise.html"));
-    });
+router.get("/", (req, res) => {
+ res.sendFile(path.join(__dirname,"../public/index.html"))
+});
 
-    app.get("/stats", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/stats/html"));
-    });
-})
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname,"../public/stats.html"))
+});
+
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname,"../public/exercise.html"))
+});
+
+
+
+module.exports = router;
