0ad7dbf219f06d034fc20bd838611615b0086a3d
Liam B <71141670+liambambery23@users.noreply.github.com>
2021-02-09 11:29:40 -0500

    added api routes for stats and workouts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,51 @@
-let db = require(".../models");
+// let db = require(".../models");
 
-module.exports(function(app) {
-    app.get("/api/workouts", (req, res) => {
+// module.exports(function(app) {
+//     app.get("/api/workouts", (req, res) => {
 
+//     });
+
+//     app.post("/api/workouts", (req, res) => {
+
+//     });
+
+//     app.put("/api/workouts/:id", (req, res) => {
+
+//     });
+
+
+//     app.get("/api/workouts/range", (req, res) => {
+
+//     });
+// })
+
+const router = require("express").Router();
+const path = require("path");
+const Workout = require("../models/workout.js")
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find()
+    .limit(7)
+    .sort({'day': -1})
+    .then(function(data){
+        res.json(data)
+    })
+});
+
+router.get("/api/workouts", (req, res) => {
+   Workout.find()
+   .sort({"day": 1})
+   .then(function(data) {
+        res.json(data)
+   });
+});
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create()
+    .then(function(data) {
+        res.json(data)
     });
-
-    app.post("/api/workouts", (req, res) => {
-
-    });
-
-    app.put("/api/workouts/:id", (req, res) => {
-
-    });
+});
 
 
-    app.get("/api/workouts/range", (req, res) => {
-
-    });
-})
+module.exports = router;
