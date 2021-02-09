dfd56a35b387d6a856465e3a552abc8c32958fed
Liam B <71141670+liambambery23@users.noreply.github.com>
2021-02-09 09:53:42 -0500

    added gitignore file and framework for api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,20 @@
+let db = require(".../models");
+
+module.exports(function(app) {
+    app.get("/api/workouts", (req, res) => {
+
+    });
+
+    app.post("/api/workouts", (req, res) => {
+
+    });
+
+    app.put("/api/workouts/:id", (req, res) => {
+
+    });
+
+
+    app.get("/api/workouts/range", (req, res) => {
+
+    });
+})
